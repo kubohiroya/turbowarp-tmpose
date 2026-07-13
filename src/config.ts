c901d763f9ffe8ff1ee35fcf77f26a7189df0059
Hiroya Kubo <hiroya@cuc.ac.jp>
2026-07-13 17:59:51 +0900

    Add extension metadata

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const extensionConfig = {
+  id: 'tmpose',
+  slug: 'tmpose',
+  name: 'TMPose',
+  description: 'Use Teachable Machine Pose models for camera-based pose recognition in TurboWarp.',
+  author: 'Hiroya Kubo',
+  license: 'MPL-2.0',
+  unsandboxed: true
+} as const;
